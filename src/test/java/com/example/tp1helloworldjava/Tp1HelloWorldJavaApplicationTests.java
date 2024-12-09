313b4a1106b4d295a492326ab7aa1bf1b01e9de6
Potowai <81572547+Potowai@users.noreply.github.com>
2024-12-09 12:08:35 +0100

    Initial commit

---
+++ src/test/java/com/example/tp1helloworldjava/Tp1HelloWorldJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp1helloworldjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1HelloWorldJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
