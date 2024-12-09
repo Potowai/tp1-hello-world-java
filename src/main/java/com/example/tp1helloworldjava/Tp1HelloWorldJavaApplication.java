a4ebd79ade7895e7a16378ede13894dc3560b6b3
Potowai <81572547+Potowai@users.noreply.github.com>
2024-12-09 12:23:07 +0100

    home route

--- src/main/java/com/example/tp1helloworldjava/Tp1HelloWorldJavaApplication.java
+++ src/main/java/com/example/tp1helloworldjava/Tp1HelloWorldJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Tp1HelloWorldJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Tp1HelloWorldJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Tp1HelloWorldJavaApplication.class, args);
+    }
 
 }
